5b43b148d8d00676bad1bf811fbe56343be3af4c
Aisha S <a_shaheed@outlook.com>
2020-03-20 06:21:02 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,4 +4,4 @@ def display_rainbow(colors)
   puts 'R:#{colors[0]}', 'O:#{colors[1]}', 'Y:#{colors[2]}', 'G:#{colors[3]}', 'B:#{colors[4]}', 'I:#{colors[5]}', 'V:#{colors[6]}'
 end
 
-display_rainbow
+display_rainbow(color[0], color[1], color[2], color[3], color[4],color[5], color[6])
