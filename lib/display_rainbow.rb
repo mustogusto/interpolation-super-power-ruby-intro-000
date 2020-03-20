8307936ace994a7e09093fc948766c5818ce9db9
Aisha S <a_shaheed@outlook.com>
2020-03-20 06:06:52 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,5 +3,3 @@ def display_rainbow(colors)
   colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
   puts 'R:#{colors[0]}', 'O:#{colors[1]}', 'Y:#{colors[2]}', 'G:#{colors[3]}', 'B:#{colors[4]}', 'I:#{colors[5]}', 'V:#{colors[6]}'
 end
-
-display_rainbow('colors[0]', 'colors[1]', 'colors[2]', 'colors[3]', 'colors[4]', 'colors[5]', 'colors[6]')
